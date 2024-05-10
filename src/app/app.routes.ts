13c7b60c7e69bc140b3d4aa63b5ff24c742219c0
Sanna Olsson <saol1901@student.miun.se>
2024-05-10 12:25:05 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ConvertComponent } from './convert/convert.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'convert', component: ConvertComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, //ist för att komma till en tom sida
+  { path: '404', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/convert/convert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convert',
+  standalone: true,
+  imports: [],
+  templateUrl: './convert.component.html',
+  styleUrl: './convert.component.scss'
+})
+export class ConvertComponent {
+
+}
